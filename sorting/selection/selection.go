1f4ac49655a4e064dbf7794f30bb3a127a606c47
carp533 <olexyz@gmx.net>
2024-04-15 16:17:21 +0200

    kw16

--- sorting/selection/selection.go
+++ sorting/selection/selection.go
@@ -10,7 +10,28 @@ Ende das ganze Array sortiert.*/
 
 func Sort(a []int) []int {
 	for i := 0; i < len(a)-1; i++ {
-		// minIndex := i
+		minIndex := i
+		for j := i + 1; j < len(a); j++ {
+			if a[j] < a[minIndex] {
+				minIndex = j
+			}
+		}
+		a[i], a[minIndex] = a[minIndex], a[i]
 	}
 	return a
 }
+
+// LÖSUNG:
+
+// func Sort(a []int) []int {
+// 	for i := 0; i < len(a)-1; i++ {
+// 		minIndex := i
+// 		for j := i + 1; j < len(a); j++ {
+// 			if a[j] < a[minIndex] {
+// 				minIndex = j
+// 			}
+// 		}
+// 		a[i], a[minIndex] = a[minIndex], a[i]
+// 	}
+// 	return a
+// }
